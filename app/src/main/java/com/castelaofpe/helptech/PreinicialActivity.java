f61c5da1f3d6d58d791bf3c0f5e1237231d35c3b
Juan Varela Mourelle <juancontactovm@gmail.com>
2020-10-28 18:04:42 +0100

    ADD : SplashScreen(congelar imagen por un momento) en PreinicialActivity

--- app/src/main/java/com/castelaofpe/helptech/PreinicialActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/PreinicialActivity.java
@@ -2,15 +2,31 @@ package com.castelaofpe.helptech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class PreinicialActivity extends AppCompatActivity {
 
-
+    private final int DURACION_SPLASH = 2000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_preinicial);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.frg_recupera_pass);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(PreinicialActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURACION_SPLASH);
     }
+
 }
