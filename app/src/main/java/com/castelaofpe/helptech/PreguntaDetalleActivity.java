fd464d08b26614ded72f71c189bbd35e237c7c9d
David Barreiro Cabanas <davidbcabanas@gmail.com>
2020-10-30 19:02:30 +0100

    ADD: InicialActivity creada ADD: nuevos estilos de botnones

---
+++ app/src/main/java/com/castelaofpe/helptech/PreguntaDetalleActivity.java
@@ -0,0 +1,14 @@
+package com.castelaofpe.helptech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreguntaDetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_pregunta_detalle);
+    }
+}
