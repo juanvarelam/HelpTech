b98cc803519c22452fcc1b173ac3f2021a6e69cb
David Barreiro Cabanas <davidbcabanas@gmail.com>
2020-11-02 20:35:09 +0100

    ADD: añado campos a Leguaje.java

--- app/src/main/java/com/castelaofpe/helptech/models/Leguaje.java
+++ app/src/main/java/com/castelaofpe/helptech/models/Leguaje.java
@@ -2,4 +2,22 @@ package com.castelaofpe.helptech.models;
 
 public class Leguaje {
 
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
