88e042e77bd57f89b497129fe167fd97708fe475
David Barreiro Cabanas <davidbcabanas@gmail.com>
2020-11-05 13:06:01 +0100

    ADD: creo InicialFragment y cambio los widgets al gragment nuevo y en InicialActivity dejo el container donde carguen los fragments. Camvio codigo en ambas pantallas

--- app/src/main/java/com/castelaofpe/helptech/inicio/InicialActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/inicio/InicialActivity.java
@@ -12,7 +12,7 @@ import com.castelaofpe.helptech.R;
 
 public class InicialActivity extends AppCompatActivity {
 
-    LoginFragment frgLog    = new LoginFragment();
+    InicialFragment frgInicial    = new InicialFragment();
     RegisterFragment frgReg = new RegisterFragment();
 
     @Override
@@ -20,33 +20,14 @@ public class InicialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_inicial);
 
-        //si pulsa SIGN IN
-        Button btnLogin = findViewById(R.id.act_inicial_btn_inicio);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction trans = manager.beginTransaction();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction trans = manager.beginTransaction();
 
-                trans.replace(R.id.act_auth_container_frame, frgLog, "fragment_01");
-
-                trans.commit();
-            }
-        });
+        trans.replace(R.id.act_auth_container_frame, frgInicial, "fragment_01");
+        trans.commit();
 
 
-        Button btnRegister = findViewById(R.id.act_inicial_btn_registro);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction trans = manager.beginTransaction();
 
-                trans.replace(R.id.act_auth_container_frame, frgReg, "fragment_01");
-
-                trans.commit();
-            }
-        });
         
 
 
