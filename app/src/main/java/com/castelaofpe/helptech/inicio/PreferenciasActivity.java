7c5f545c66b97183bc4ee9c077d751e2472d590e
Juan Varela Mourelle <juancontactovm@gmail.com>
2020-11-07 09:41:31 +0100

    ADD: enlace adaptador + termino configurar adapter

--- app/src/main/java/com/castelaofpe/helptech/inicio/PreferenciasActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/inicio/PreferenciasActivity.java
@@ -1,16 +1,35 @@
 package com.castelaofpe.helptech.inicio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.castelaofpe.helptech.R;
 
+import java.util.ArrayList;
+
 public class PreferenciasActivity extends AppCompatActivity {
+
+    ArrayList<String> listPreferencias;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_preferencias);
+
+        recycler = (RecyclerView) findViewById(R.id.recycler_preferencias);
+        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false) );
+
+        listPreferencias = new ArrayList<String>();
+
+        for (int i=0; i<50; i++) {
+            listPreferencias.add("Item # " +i+" ");
+        }
+
+        AdapterPreferencias adapter = new AdapterPreferencias(listPreferencias);
+        recycler.setAdapter(adapter);
     }
 }
