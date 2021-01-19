c5209261568335773e095354c93c3726f3d4e87f
Laura <laura.calespineira@gmail.com>
2021-01-19 21:55:58 +0100

    ADD: Dialogs

--- app/src/main/java/com/castelaofpe/helptech/perfil/ConfigFragment.java
+++ app/src/main/java/com/castelaofpe/helptech/perfil/ConfigFragment.java
@@ -1,66 +1,143 @@
 package com.castelaofpe.helptech.perfil;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.castelaofpe.helptech.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConfigFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ConfigFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button btnNombre;
+    private Button btnPass;
+    private Button btnEmail;
+    private Button btnPrivacidad;
+    private Button btnFoto;
 
     public ConfigFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfigFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConfigFragment newInstance(String param1, String param2) {
-        ConfigFragment fragment = new ConfigFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frg_config, container, false);
+        View v = inflater.inflate(R.layout.frg_config, container, false);
+        initViews(v);
+        initListeners();
+        return v;
+    }
+
+    private void initViews(View v) {
+        btnNombre = v.findViewById(R.id.frg_config_nombre_btn);
+        btnPass = v.findViewById(R.id.frg_config_pass_btn);
+        btnPrivacidad = v.findViewById(R.id.frg_config_privacidad_btn);
+    }
+
+    private void initListeners() {
+
+        btnNombre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogUsuario();
+            }
+            private void showDialogUsuario() {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+                View v = inflater.inflate(R.layout.dialog_usuario, null);
+                final EditText edtNombre = v.findViewById(R.id.dialog_usuario_nombre_edt);
+
+                builder.setView(v)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                String nombre = edtNombre.getText().toString();
+                                Log.i("Nuevo nombre",nombre);
+
+                            }
+                        }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        btnPass.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                showDialogPass();
+            }
+
+            private void showDialogPass() {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+                View v = inflater.inflate(R.layout.dialog_password, null);
+                final EditText edtNuevaPass= v.findViewById(R.id.dialog_pass_nueva_edt);
+                final EditText edtConfirmaPass= v.findViewById(R.id.dialog_pass_confirma_edt);
+
+                builder.setView(v)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                String nuevaPass = edtNuevaPass.getText().toString();
+                                String confirmaPass = edtConfirmaPass.getText().toString();
+
+                                if( nuevaPass.equals(confirmaPass) )//Guardar contraseña
+                                    Log.i("Contraseña guardada", nuevaPass);
+                            }
+                        }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        btnPrivacidad.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                showDialogPrivacidad();
+            }
+
+            private void showDialogPrivacidad() {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+                View v = inflater.inflate(R.layout.dialog_privacidad, null);
+                final TextView txtPrivacidad = v.findViewById(R.id.dialog_privacidad_txt);
+
+                builder.setView(v)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
     }
 }
