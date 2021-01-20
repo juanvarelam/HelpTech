0618602066956d09ce5b9b2007a03d1f0158068a
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-01-20 12:51:30 +0100

    FIX: rediseño act_perfil.xml y modifico todos los adapters

--- app/src/main/java/com/castelaofpe/helptech/perfil/AdapterPager.java
+++ app/src/main/java/com/castelaofpe/helptech/perfil/AdapterPager.java
@@ -1,36 +1,48 @@
 package com.castelaofpe.helptech.perfil;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdapterPager extends FragmentPagerAdapter {
 
-    int numTabs;
+    private final List<Fragment> lstFragment = new ArrayList<>();
+    private final List<String> lstTitles = new ArrayList<>();
 
-    public AdapterPager(@NonNull FragmentManager fm, int behavior) {
+    public AdapterPager(@NonNull FragmentManager fm, Integer behavior) {
         super(fm, behavior);
-        this.numTabs = behavior;
+    }
+
+    public AdapterPager(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return new AdapterPreguntas();
-            case 1:
-                return new AdapterRespuestas();
-            case 2:
-                return new AdapterFavoritos();
-            default:
-                return null;
-        }
+        return lstFragment.get(position);
     }
 
     @Override
     public int getCount() {
-        return numTabs;
+        return lstTitles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return lstTitles.get(position);
+    }
+
+
+    public void AddFragment (Fragment fragment, String title) {
+        lstFragment.add(fragment);
+        lstTitles.add(title);
     }
 }
--- app/src/main/java/com/castelaofpe/helptech/perfil/PerfilActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/perfil/PerfilActivity.java
@@ -11,49 +11,35 @@ import com.google.android.material.tabs.TabLayout;
 
 public class PerfilActivity extends AppCompatActivity {
 
-    TabLayout tabLAyout;
-    ViewPager viewPager;
-    TabItem tab1, tab2, tab3;
-    AdapterPager adapterPager;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private AdapterPager adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_perfil);
-        tabLAyout = findViewById(R.id.act_perfil_tabLayout);
-        viewPager = findViewById(R.id.act_perfil_viewPager);
 
-        tab1 = findViewById(R.id.act_perfil_tabPreguntas);
-        tab2 = findViewById(R.id.act_perfil_tabRespuestas);
-        tab3 = findViewById(R.id.act_perfil_tabFavoritos);
+        tabLayout = (TabLayout) findViewById(R.id.act_perfil_tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.act_perfil_viewPager);
+        adapter = new AdapterPager(getSupportFragmentManager());
 
-        adapterPager = new AdapterPager(getSupportFragmentManager(), tabLAyout.getTabCount());
-        viewPager.setAdapter(adapterPager);
-        tabLAyout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-                if (tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 1) {
-                    adapterPager.notifyDataSetChanged();
-                }
-                if (tab.getPosition() == 1) {
-                    adapterPager.notifyDataSetChanged();
-                }
-                if (tab.getPosition() == 2) {
-                    adapterPager.notifyDataSetChanged();
-                }
-            }
+        //Add Fragment Here
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
+        adapter.AddFragment(new AdapterPreguntas(),"Preguntas");
+        adapter.AddFragment(new AdapterRespuestas(),"Respuestas");
+        adapter.AddFragment(new AdapterFavoritos(),"");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
 
-            }
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_action_favorite);
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        });
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLAyout));
+
+
     }
+
 }
