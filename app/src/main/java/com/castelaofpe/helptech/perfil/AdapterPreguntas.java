72a5e42a1fef3126a0be0359f20bde5af321d54e
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-01-21 12:22:14 +0100

    ADD: adaptador recyclerView de tabLayout, clase Pregunta FIX: modifico tabLayout de act_perfil (crashea)

--- app/src/main/java/com/castelaofpe/helptech/perfil/AdapterPreguntas.java
+++ app/src/main/java/com/castelaofpe/helptech/perfil/AdapterPreguntas.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +14,15 @@ import android.view.ViewGroup;
 
 import com.castelaofpe.helptech.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class AdapterPreguntas extends Fragment {
 
     View v;
+    private RecyclerView myrecyclerview;
+    private List<Pregunta> lstPregunta;
 
     public AdapterPreguntas() {
 
@@ -25,6 +32,23 @@ public class AdapterPreguntas extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.frg_preguntas, container, false);
+        myrecyclerview = (RecyclerView) v.findViewById(R.id.frg_preguntas_recyclerPreguntas);
+        AdapterRecyclerTabsPerfil recyclerViewAdapter = new AdapterRecyclerTabsPerfil(getContext(),lstPregunta);
+        myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myrecyclerview.setAdapter(recyclerViewAdapter);
         return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        lstPregunta = new ArrayList<>();
+        lstPregunta.add(new Pregunta("10:24", "¿Cómo usar el mejor framework em Java?", 8, 12, "#java, #framework"));
+        lstPregunta.add(new Pregunta("8:20", "¿Cómo aprender a programar en el menor tiempo posible?", 4, 1, "#programar"));
+        lstPregunta.add(new Pregunta("Ayer", "¿Cómo conectar MySQL con Java?", 1, 23, "#java, #mysql"));
+        lstPregunta.add(new Pregunta("Martes", "¿Cómo alinear texto en Android Studio?", 7, 42, "#androidstudio, #alinear"));
+        lstPregunta.add(new Pregunta("Martes", "¿Cómo subir los cambios a Git?", 9, 41, "#git, #sourcetree"));
+        lstPregunta.add(new Pregunta("Martes", "¿Cómo importar dependencias en el pom?", 3, 71, "#eclipse, #pom"));
     }
 }
---
+++ app/src/main/java/com/castelaofpe/helptech/perfil/AdapterRecyclerTabsPerfil.java
@@ -0,0 +1,71 @@
+package com.castelaofpe.helptech.perfil;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.castelaofpe.helptech.R;
+
+import java.util.List;
+
+public class AdapterRecyclerTabsPerfil extends RecyclerView.Adapter<AdapterRecyclerTabsPerfil.MyViewHolder> {
+
+
+    Context mContext;
+    List<Pregunta> mData;
+
+    public AdapterRecyclerTabsPerfil(Context mContext, List<Pregunta> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public AdapterRecyclerTabsPerfil.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        v = LayoutInflater.from(mContext).inflate(R.layout.item_preguntas, parent, false);
+        MyViewHolder vHolder = new MyViewHolder(v);
+        return vHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterRecyclerTabsPerfil.MyViewHolder holder, int position) {
+        holder.str_fecha.setText(mData.get(position).getFecha());
+        holder.str_texto_pregunta.setText(mData.get(position).getTexto());
+        holder.int_comentarios.setText(mData.get(position).getComentarios());
+        holder.int_votos.setText(mData.get(position).getVotos());
+        holder.str_hagstags.setText(mData.get(position).getHagstags());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        private TextView str_fecha;
+        private TextView str_texto_pregunta;
+        private TextView int_comentarios;
+        private TextView int_votos;
+        private TextView str_hagstags;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        str_fecha = (TextView) itemView.findViewById(R.id.frg_preguntas_fecha);
+        str_texto_pregunta = (TextView) itemView.findViewById(R.id.frg_preguntas_texto_preguntas);
+        int_comentarios = (TextView) itemView.findViewById(R.id.frg_preguntas_numero_comentarios);
+        int_votos = (TextView) itemView.findViewById(R.id.frg_preguntas_numero_votos);
+        str_hagstags = (TextView) itemView.findViewById(R.id.frg_preguntas_hagstags);
+
+    }
+}
+
+}
