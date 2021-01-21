46fa80eab7333ad7593a38110d6934d234cda95c
David Barreiro Cabanas <davidbcabanas@gmail.com>
2021-01-21 13:29:57 +0100

    ADD: funcionalidad a boton en RecuperaPassFragment

--- app/src/main/java/com/castelaofpe/helptech/inicio/InicialActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/inicio/InicialActivity.java
@@ -8,8 +8,6 @@ import androidx.fragment.app.FragmentTransaction;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.castelaofpe.helptech.R;
 
@@ -23,7 +21,7 @@ public class InicialActivity extends AppCompatActivity {
         setContentView(R.layout.act_inicial);
 
         changeFragmentInicio(frgInicial);
-
+        
     }
 
     public void changeFragmentInicio(Fragment frg){
