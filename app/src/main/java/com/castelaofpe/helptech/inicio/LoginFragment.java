f894d048f44fdb56de47c354c6abdfa9851b6003
David Barreiro Cabanas <davidbcabanas@gmail.com>
2021-01-30 11:48:25 +0100

    ADD: compruebo Datos en Login, muestra una alerta en caso de no ser correctos

--- app/src/main/java/com/castelaofpe/helptech/inicio/LoginFragment.java
+++ app/src/main/java/com/castelaofpe/helptech/inicio/LoginFragment.java
@@ -2,62 +2,33 @@ package com.castelaofpe.helptech.inicio;
 
 import android.os.Bundle;
 
+import androidx.annotation.StringDef;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.castelaofpe.helptech.R;
 import com.castelaofpe.helptech.principal.MainActivity;
 
-/*
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.DataOutputStream;
+
+
 public class LoginFragment extends Fragment {
-/*
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText email, password;
 
-    public LoginFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      /*  if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }*/
+
 
     }
 
@@ -71,12 +42,16 @@ public class LoginFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.frg_login, container, false);
 
+        email = v.findViewById(R.id.frg_login_email);
+        password = v.findViewById(R.id.frg_login_password);
+
         //si accede a la aplicacion
         Button btnSignIn = v.findViewById(R.id.frg_login_btn_sigin);
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity actMain = new MainActivity();
+                checkDatos();
                 ((InicialActivity)getActivity()).iniciaActivity(actMain);
             }
         });
@@ -101,4 +76,29 @@ public class LoginFragment extends Fragment {
 
         return v;
     }
+
+    private void checkDatos(){
+
+        String error = getString(R.string.datos_incorrectos);
+
+        Toast toast = Toast.makeText(getContext(), error,Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP,0,0);
+
+        String compruebaEmail = email.getText().toString();
+        if(compruebaEmail.trim().isEmpty()){
+            toast.show();
+            return;
+        }
+        String compruebaPassword = password.getText().toString();
+        if(compruebaPassword.trim().isEmpty()){
+            toast.show();
+            return;
+        }
+
+
+
+
+    }
+
+
 }
