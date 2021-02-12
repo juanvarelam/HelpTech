9ccd4c1861c2b94a7bb752a9eaa48aa0d821b27c
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-02-12 21:14:59 +0100

    ADD: act_foto y act_descripcion ADD: imagen camara en drawable y fondo_imput_descripcion

---
+++ app/src/main/java/com/castelaofpe/helptech/inicio/DescripcionActivity.java
@@ -0,0 +1,16 @@
+package com.castelaofpe.helptech.inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.castelaofpe.helptech.R;
+
+public class DescripcionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_descripcion);
+    }
+}
---
+++ app/src/main/java/com/castelaofpe/helptech/inicio/FotoActivity.java
@@ -0,0 +1,16 @@
+package com.castelaofpe.helptech.inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.castelaofpe.helptech.R;
+
+public class FotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_foto);
+    }
+}
