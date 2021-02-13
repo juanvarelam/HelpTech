244d3a6ee98115958ff1077b5f4d24add43bdc22
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-02-13 11:22:52 +0100

    ADD: conecto foto_activity y descripcion_act con los demás activitys

--- app/src/main/java/com/castelaofpe/helptech/inicio/DescripcionActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/inicio/DescripcionActivity.java
@@ -2,9 +2,12 @@ package com.castelaofpe.helptech.inicio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.castelaofpe.helptech.R;
+import com.castelaofpe.helptech.principal.MainActivity;
 
 public class DescripcionActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class DescripcionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_descripcion);
+    }
+
+
+    public void onClick(View view) {
+        Intent vista = new Intent(DescripcionActivity.this, MainActivity.class);
+        startActivity(vista);
     }
 }
--- app/src/main/java/com/castelaofpe/helptech/inicio/FotoActivity.java
+++ app/src/main/java/com/castelaofpe/helptech/inicio/FotoActivity.java
@@ -2,9 +2,12 @@ package com.castelaofpe.helptech.inicio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.castelaofpe.helptech.R;
+import com.castelaofpe.helptech.principal.MainActivity;
 
 public class FotoActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class FotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_foto);
+    }
+
+
+    public void onClick(View view) {
+        Intent vista = new Intent(FotoActivity.this, DescripcionActivity.class);
+        startActivity(vista);
     }
 }
